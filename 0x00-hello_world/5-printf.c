c00593fdc3b7ddba3a390d5191159c7a2566fe7f
ChukwudiOkereke <bobclain09@yahoo.com>
2022-09-11 07:48:50 -0700

    debung

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
@@ -15,9 +13,7 @@
 int main(void)
 
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
-
 }
